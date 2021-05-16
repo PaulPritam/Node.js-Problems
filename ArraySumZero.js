a3e92da5dfb40d1e2bd5a075cc4473e5ee395f7d
unknown <pritampaul9797@gmail.com>
2021-05-16 10:02:04 +0530

    Added: Array program that shows sum of numbers is zero

---
+++ ArraySumZero.js
@@ -0,0 +1,18 @@
+const input = require("readline-sync");
+
+var arr = [1,1,-2];
+const arr = Array(2);
+var sum = 0;
+for(var i = 0; i<arr.length; i++)
+{
+    sum += arr[i];    
+}
+
+if(sum==0)
+{
+    console.log("The sum is zero")
+}
+else
+{
+    console.log("non-zero")
+}
